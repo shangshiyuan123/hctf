fcf6c07aec929d05ae7bad51fb9036295725bbb3
Administrator <787702891@qq.com>
2020-03-24 21:30:35 +0800

    添加书籍数据层

---
+++ src/main/java/com/javakc/cms/modules/book/dao/BookDao.java
@@ -0,0 +1,7 @@
+package com.javakc.cms.modules.book.dao;
+
+import com.javakc.cms.base.dao.BaseDao;
+import com.javakc.cms.modules.book.entity.Book;
+
+public interface BookDao extends BaseDao<Book> {
+}
